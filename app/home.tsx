c0370863fb55d80e5eab5129cfdb5264562e96a7
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2025-01-09 22:36:32 +0530

    Pie chart

--- app/home.tsx
+++ app/home.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import PieChart from "react-native-pie-chart";
 
 export default function Home() {
+  const widthAndHeight = 250;
+  const data = [
+    { value: 430, color: "#fbd203", label: { text: 'A', fontWeight: 'bold' } },
+    { value: 50, color: "#ffb300", label: { text: 'A', fontWeight: 'bold' } },
+    { value: 185, color: "#ff9100" , label: { text: 'A', fontWeight: 'bold' }},
+    { value: 123, color: "#ff6c00" , label: { text: 'A', fontWeight: 'bold' }},
+  ];
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.top}>
         <Text style={styles.expen}>Expenses</Text>
         <View style={styles.total}>
-          <Text style={styles.totalText} onPress={() => console.log("Text Pressed")}>
+          <Text
+            style={styles.totalText}
+            onPress={() => console.log("Text Pressed")}
+          >
             Rs 53183
           </Text>
         </View>
       </View>
+      <Text style={styles.baltext}>Balance</Text>
       <View style={styles.balance}>
-          <Text style={styles.baltext}>Balance</Text>
+        <Text style={styles.upi}>Rs 53183</Text>
+        <Text style={styles.cash}>Rs 53183</Text>
+      </View>
+      <View style={styles.piechart}>
+      <PieChart
+          widthAndHeight={widthAndHeight}
+          series={data}
+        />
       </View>
     </SafeAreaView>
   );
@@ -22,7 +41,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0f0f0f", // Set background color
+    backgroundColor: "#2e2e2e", // Set background color
     paddingTop: 50,
     paddingHorizontal: 20,
   },
@@ -35,7 +54,7 @@ const styles = StyleSheet.create({
   },
   total: {
     backgroundColor: "#FFF4B7",
-    padding:10,
+    padding: 10,
     borderRadius: 10,
     width: 150,
   },
@@ -43,7 +62,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#000B58",
     fontWeight: "bold",
-    textAlign:"center",
+    textAlign: "center",
   },
   expen: {
     fontSize: 34,
@@ -51,12 +70,40 @@ const styles = StyleSheet.create({
     color: "#FFF4B7",
     width: 150,
   },
-  balance:{
-    
+  balance: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
   },
-  baltext:{
-    fontSize:20,
-    marginTop:15,
+  upi:{
+    backgroundColor: "#FFF4B7",
+    padding: 15,
+    borderRadius: 10,
+    width: "48%",
+    fontFamily: "Roboto",
+    textAlign: "center",
+  },
+  cash:{
+    backgroundColor: "#FFF4B7",
+    padding: 15,
+    borderRadius: 10,
+    width: "48%",
+    textAlign: "center",
+  },
+  baltext: {
+    fontSize: 15,
+    marginTop: 20,
     color: "white",
+  },
+  piechart: {
+    backgroundColor: "rgb(53, 51, 51)",
+    padding: 15,
+    paddingTop: 25,
+    borderRadius: 10,
+    width: "100%",
+    height: 300,
+    marginTop: 20,
+    alignItems: "center",
   },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#0f0f0f",
+    backgroundColor: "#2e2e2e",
   },
   text: {
     fontSize: 80,
